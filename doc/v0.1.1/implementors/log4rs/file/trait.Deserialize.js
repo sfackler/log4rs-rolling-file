1653a778474cd16130c17aefad45253d2ac9bcfc
Steven Fackler <sfackler@palantir.com>
2016-05-25 09:16:14 -0700

    Release v0.1.1

---
+++ doc/v0.1.1/implementors/log4rs/file/trait.Deserialize.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['log4rs_rolling_file'] = ["impl <a class='trait' href='https://sfackler.github.io/log4rs/doc/v0.4.3/log4rs/file/trait.Deserialize.html' title='log4rs::file::Deserialize'>Deserialize</a> for <a class='struct' href='log4rs_rolling_file/roll/delete/struct.DeleteRollerDeserializer.html' title='log4rs_rolling_file::roll::delete::DeleteRollerDeserializer'>DeleteRollerDeserializer</a>","impl <a class='trait' href='https://sfackler.github.io/log4rs/doc/v0.4.3/log4rs/file/trait.Deserialize.html' title='log4rs::file::Deserialize'>Deserialize</a> for <a class='struct' href='log4rs_rolling_file/roll/fixed_window/struct.FixedWindowRollerDeserializer.html' title='log4rs_rolling_file::roll::fixed_window::FixedWindowRollerDeserializer'>FixedWindowRollerDeserializer</a>","impl <a class='trait' href='https://sfackler.github.io/log4rs/doc/v0.4.3/log4rs/file/trait.Deserialize.html' title='log4rs::file::Deserialize'>Deserialize</a> for <a class='struct' href='log4rs_rolling_file/trigger/size/struct.SizeTriggerDeserializer.html' title='log4rs_rolling_file::trigger::size::SizeTriggerDeserializer'>SizeTriggerDeserializer</a>","impl <a class='trait' href='https://sfackler.github.io/log4rs/doc/v0.4.3/log4rs/file/trait.Deserialize.html' title='log4rs::file::Deserialize'>Deserialize</a> for <a class='struct' href='log4rs_rolling_file/struct.RollingFileAppenderDeserializer.html' title='log4rs_rolling_file::RollingFileAppenderDeserializer'>RollingFileAppenderDeserializer</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
